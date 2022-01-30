b6d56bd89254b0f82546614ea6a47c24db199500
tkki_iOSX <tkki_iOSX@icloud.com>
2022-01-30 17:40:26 +0900

    Initial Commit

---
+++ tabView/tabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabViewApp.swift
+//  tabView
+//
+//  Created by Xcode2021 on 2022/01/30.
+//
+
+import SwiftUI
+
+@main
+struct tabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
